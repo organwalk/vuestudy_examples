531b1c089e9bf1917c2861b36211d7ea58375cff
haruki <harukisea0@gmail.com>
2023-03-18 11:23:48 +0800

    enter

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,25 @@
     <HandingInput></HandingInput>
     <AttributeBindings></AttributeBindings>
     <Conditionals_And_Loops></Conditionals_And_Loops>
+    <FormBindings></FormBindings>
+    <TodoItem v-for="item in groceryList" :todo="item" :key="item.id"></TodoItem>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue';
 import HandingInput from './components/Handing-Input.vue';
 import AttributeBindings from './components/Attribute-Bindings.vue';
 import Conditionals_And_Loops from './components/Conditionals_And_Loops.vue';
+import FormBindings from './components/Form-Bindings.vue';
+import TodoItem from './components/TodoItem.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    HandingInput,
-    AttributeBindings,
-    Conditionals_And_Loops,
-  }
-}
+const groceryList = ref([
+  { id: 0, text: 'Vegetables' },
+  { id: 1, text: 'Cheese' },
+  { id: 2, text: 'Whatever else humans are supposed to eat' }
+])
 </script>
 
 <style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,11 @@
+<template>
+    <li>{{ todo.text }}</li>
+</template>
+
+<script setup>
+import { defineProps } from '@vue/runtime-core';
+    defineProps({
+        todo:Object
+    })
+
+</script>
